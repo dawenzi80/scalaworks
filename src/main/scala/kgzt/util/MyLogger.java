88b95bcc17dcca415cdbfe926872208dd2c230d2
liuxw <liuxw@qzt360.com>
2017-06-23 18:44:33 +0800

    Liuxw 20170623

---
+++ src/main/scala/kgzt/util/MyLogger.java
@@ -0,0 +1,11 @@
+package kgzt.util;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+/**
+ * Created by liuxw on 2017/6/21.
+ */
+public class MyLogger {
+    public static Logger LOGGER = Logger.getLogger("kgzt");
+}
